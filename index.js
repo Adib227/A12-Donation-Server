fed64f8101eb6d2a387b64ce8380ad6112089906
Iftekhar Aziz Adib <iftekharadib227@gmail.com>
2024-06-21 22:17:59 +0600

    added middleware

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0xksggv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
