4c6ea1170189a3a56f5d3c3adc09f4a47da9c408
Iftekhar Aziz Adib <iftekharadib227@gmail.com>
2024-12-09 01:15:09 +0600

    runned users collection

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('server is running');
+  res.send('server is running smoothly');
 });
 
 app.listen(port, () => {
